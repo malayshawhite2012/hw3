b3a53b7037e33b792818641e9950af0248150b14
Malaysha White <dajour@umich.edu>
2016-11-08 18:15:50 -0500

    Added a # to the doument

--- bshw3.py
+++ bshw3.py
@@ -10,6 +10,7 @@
 
 # Deliverables
 # Make sure the new page is uploaded to your GitHub account.
+#
 print("i should start my homework")
 print("start with homework 2 part C")
 print("use the string matching from homework1")
