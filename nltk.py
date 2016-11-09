e97a2bb884f58cde7748b33e22e799c2b9e48be6
Malaysha White <dajour@umich.edu>
2016-11-08 19:38:21 -0500

    Added Nltk

---
+++ nltk.py
@@ -0,0 +1,7 @@
+
+from nltk.tokenize import sent_tokenize, word_tokenize
+text = "Hello students, how are you doing today? Have you recovered from the exam?  I hope you are feeling better.  Things will be fine."
+print(sent_tokenize(text)) 
+print(word_tokenize(text))
+for i in word_tokenize(text):
+	print(i)
