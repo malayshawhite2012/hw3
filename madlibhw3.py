6671e6b1256342397aa09a299a2c39d0ea5b5e79
Malaysha White <dajour@umich.edu>
2016-11-10 15:35:20 -0500

    Added part1 of homework

--- madlibhw3.py
+++ madlibhw3.py
@@ -3,13 +3,47 @@
 
 # Requirements:
 # 1) Only use the first 150 tokens
-# 2) Pick 5 parts of speech to prompt for, including nouns
+# 2) , 
 # 3) Replace nouns 15% of the time, everything else 10%
 
 # Deliverables:
 # 1) Print the orginal text (150 tokens)
 # 1) Print the new text
-print("START*******")
+
+# code developed by Jackie Cohen; revised by Paul Resnick
+# further revised by Colleen van Lent for Python3
+# requires some downloading/installing dependencies to use all its features; numpy is especially tricky to install
+import nltk
+from nltk.book import text2
+import random
+
+# import nltk
+# nltk.download('punkt')
+
+from nltk import word_tokenize,sent_tokenize
+
+tokens = text2[:150]
+print(tokens)
+tagged_tokens = nltk.pos_tag(tokens) # gives us a tagged list of tuples
 
 
-print("\n\nEND*******")
+tagmap = {"NN":"a noun","NNS":"a plural noun","VB":"a verb","JJ":"an adjective","PRP":" a preposition"}
+substitution_probabilities = {"NN":.15,"NNS":.1,"VB":.1,"JJ":.1, "PRP": .1}
+
+def spaced(word):
+	if word in [",", ".", "?", "!", ":"]:
+		return word
+	else:
+		return " " + word
+
+final_words = []
+
+
+for (word, tag) in tagged_tokens:
+	if tag not in substitution_probabilities or random.random() > substitution_probabilities[tag]:
+		final_words.append(spaced(word))
+	else:
+		new_word = input("Please enter %s:\n" % (tagmap[tag]))
+		final_words.append(spaced(new_word))
+
+print ("".join(final_words))
